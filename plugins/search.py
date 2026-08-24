bc1c6bf61ec2668489553ac3915a7875ffc1003a
Jani Hirvinen <jphelirc@gmail.com>
2026-08-24 21:20:13 +0300

    ki-search: move version/copyright row to bottom-right

--- plugins/search.py
+++ plugins/search.py
@@ -38,17 +38,6 @@ class SearchDialog(wx.Dialog):
         panel = wx.Panel(self)
         vbox = wx.BoxSizer(wx.VERTICAL)
 
-        # Row 0: version + copyright with clickable link
-        hbox_top = wx.BoxSizer(wx.HORIZONTAL)
-        hbox_top.Add(wx.StaticText(panel, label="Ki-Search V{}".format(plugin_version)),
-                     flag=wx.ALIGN_CENTER_VERTICAL | wx.RIGHT, border=10)
-        hbox_top.Add(wx.StaticText(panel, label="(c)2026 "),
-                     flag=wx.ALIGN_CENTER_VERTICAL)
-        hbox_top.Add(wx.adv.HyperlinkCtrl(panel, wx.ID_ANY, "jpkh/fi",
-                                          "https://github.com/jpkh"),
-                     flag=wx.ALIGN_CENTER_VERTICAL)
-        vbox.Add(hbox_top, flag=wx.LEFT | wx.RIGHT | wx.TOP, border=10)
-
         # Row 1: search + actions
         hbox1 = wx.BoxSizer(wx.HORIZONTAL)
         self.search_ctrl = wx.TextCtrl(panel, size=(400, -1), style=wx.TE_PROCESS_ENTER)
@@ -75,6 +64,18 @@ class SearchDialog(wx.Dialog):
         self.info_label = wx.StaticText(panel, label="")
         vbox.Add(self.info_label,
                  flag=wx.EXPAND | wx.LEFT | wx.RIGHT | wx.BOTTOM, border=10)
+
+        # Row 4: version + copyright bottom-right (clickable link)
+        hbox_bottom = wx.BoxSizer(wx.HORIZONTAL)
+        hbox_bottom.AddStretchSpacer(1)
+        hbox_bottom.Add(wx.StaticText(panel, label="Ki-Search V{}".format(plugin_version)),
+                        flag=wx.ALIGN_CENTER_VERTICAL | wx.RIGHT, border=10)
+        hbox_bottom.Add(wx.StaticText(panel, label="(c)2026 "),
+                        flag=wx.ALIGN_CENTER_VERTICAL)
+        hbox_bottom.Add(wx.adv.HyperlinkCtrl(panel, wx.ID_ANY, "jpkh/fi",
+                                             "https://github.com/jpkh"),
+                        flag=wx.ALIGN_CENTER_VERTICAL)
+        vbox.Add(hbox_bottom, flag=wx.EXPAND | wx.LEFT | wx.RIGHT | wx.BOTTOM, border=10)
 
         self.search_btn.Bind(wx.EVT_BUTTON, self.on_search)
         self.search_ctrl.Bind(wx.EVT_TEXT_ENTER, self.on_search)
